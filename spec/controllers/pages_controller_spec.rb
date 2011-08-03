6d8b4ca122d7f819d1aa29621d9e7a4680a48cb1
Arvind Subramanian <arvind@kappboom.com>
2011-08-02 19:28:50 -0700

    Added layout links (with routes).

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -62,6 +62,21 @@ describe PagesController do
                                     :content => "#{@base_title} | About")
     end
     
+    describe "GET 'help'" do
+
+      it "should be successful" do
+        get 'help'
+        response.should be_success
+      end
+
+      it  "should have the right title" do
+        get 'help'
+
+        response.should have_selector("title",
+                                      :content => "#{@base_title} | Help")
+      end
+    end
+    
   end
 
 end
